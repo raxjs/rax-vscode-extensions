6d19418ddfdb0772ebb094b8346e330c49fac917
yangfan <18767120422@163.com>
2020-01-08 20:57:27 +0800

    feat: add test

--- extensions/vscode-rax-styles/src/index.js
+++ extensions/vscode-rax-styles/src/index.js
@@ -1,37 +1,33 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const path = require('path');
+// const fs = require('fs');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+function provideDefinition(document, position) {
+  const fileName = document.fileName;
+  const workDir = path.dirname(fileName);
+  const word = document.getText(document.getWordRangeAtPosition(position));
+  const line = document.lineAt(position);
 
-/**
- * @param {vscode.ExtensionContext} context
- */
+  console.log('fileName: ' + fileName);
+  console.log('workDir: ' + workDir);
+  console.log('word: ' + word);
+  console.log('line: ' + line.text);
+
+  // if (/\/package\.json$/.test(fileName)) {
+  //   const json = document.getText();
+  //   if (new RegExp(`"(dependencies|devDependencies)":\\s*?\\{[\\s\\S]*?${word.replace(/\//g, '\\/')}[\\s\\S]*?\\}`, 'gm').test(json)) {
+  //     let destPath = `${workDir}/node_modules/${word.replace(/"/g, '')}/package.json`;
+  //     if (fs.existsSync(destPath)) {
+  //       return new vscode.Location(vscode.Uri.file(destPath), new vscode.Position(0, 0));
+  //     }
+  //   }
+  // }
+}
+
 function activate(context) {
-
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "vscode-rax-styles" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-    // The code you place here will be executed every time your command is executed
-
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World!');
-  });
-
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(vscode.languages.registerDefinitionProvider(['json'], {
+    provideDefinition
+  }));
 }
+
 exports.activate = activate;
-
-// this method is called when your extension is deactivated
-function deactivate() { }
-
-module.exports = {
-  activate,
-  deactivate
-}
