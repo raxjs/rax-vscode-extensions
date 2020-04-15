c40c46f9daad16cfce6ad099cd106e32b24515fd
yangfan <18767120422@163.com>
2020-04-15 14:33:59 +0800

    feat: update

--- extensions/vscode-view-rax-explorer/src/Explorer.js
+++ extensions/vscode-view-rax-explorer/src/Explorer.js
@@ -2,60 +2,66 @@ const path = require('path');
 const vscode = require('vscode');
 const isRaxProject = require('./isRaxProject');
 
+const defaultTreeData = [
+  { name: 'Document', icon: 'document' },
+  { name: 'Components', icon: 'components' },
+  { name: 'Pages', icon: 'pages' },
+  { name: 'Configs', icon: 'config' }
+];
+
 module.exports = class Explorer {
   constructor(context) {
     this.context = context;
-
-    // Updating Tree View content
-    // https://code.visualstudio.com/api/extension-guides/tree-view#updating-tree-view-content
-    this._onDidChangeTreeData = new vscode.EventEmitter();
-    this.onDidChangeTreeData = this._onDidChangeTreeData.event;
+    this.rootPath = vscode.workspace.rootPath;
 
     // Listeners
     vscode.window.onDidChangeActiveTextEditor(() => {
       this.refresh();
     });
-    vscode.workspace.onDidChangeTextDocument(() => {
-      // TODO config
-      // this.refresh();
-    });
     vscode.workspace.onDidSaveTextDocument(() => {
       this.refresh();
     });
 
-    this.refresh();
+    // Updating Tree View content
+    // https://code.visualstudio.com/api/extension-guides/tree-view#updating-tree-view-content
+    this._onDidChangeTreeData = new vscode.EventEmitter();
+    this.onDidChangeTreeData = this._onDidChangeTreeData.event;
   }
 
   refresh() {
-    this.parseTree();
-    // this._onDidChangeTreeData.fire();
+    this._onDidChangeTreeData.fire();
   }
 
-  parseTree() {
-
-  }
-
-  // https://code.visualstudio.com/api/extension-guides/tree-view#tree-data-provider
-  getChildren() {
-    if (!isRaxProject()) {
-      return [];
-    }
+  getDocumentChildren() {
     return [
-      { script: 'start', command: 'rax.start', title: 'Start a development server' },
-      { script: 'debug', command: 'rax.debug', title: 'Run and debug' },
-      { script: 'build', command: 'rax.build', title: 'Build your project' }
+      { name: 'document.jsx' }
     ];
   }
 
+  // https://code.visualstudio.com/api/extension-guides/tree-view#tree-data-provider
+  getChildren(element) {
+    if (!isRaxProject()) {
+      return [];
+    }
+    // Process tree data
+    if (!element) {
+      return defaultTreeData;
+    } else {
+      return [];
+    }
+  }
+
   getTreeItem(element) {
-    const treeItem = new vscode.TreeItem(element.script, vscode.TreeItemCollapsibleState.None);
-    treeItem.command = {
-      command: element.command,
-      title: element.title
-    };
+    const treeItem = new vscode.TreeItem(element.name, vscode.TreeItemCollapsibleState.Expanded);
+    if (element.command) {
+      treeItem.command = {
+        command: element.command,
+        title: element.title
+      };
+    }
     treeItem.iconPath = {
-      light: this.context.asAbsolutePath(path.join('assets', 'light', 'wrench.svg')),
-      dark: this.context.asAbsolutePath(path.join('assets', 'dark', 'wrench.svg'))
+      light: this.context.asAbsolutePath(path.join('assets', 'light', `${element.icon}.svg`)),
+      dark: this.context.asAbsolutePath(path.join('assets', 'dark', `${element.icon}.svg`))
     };
     return treeItem;
   }
