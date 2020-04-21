f3300bc6e88084d546ffe0c28a51ba217773fba8
yangfan <18767120422@163.com>
2020-04-21 17:42:02 +0800

    feat: add test

--- extensions/vscode-package-version-helper/src/index.js
+++ extensions/vscode-package-version-helper/src/index.js
@@ -1,36 +1,63 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const path = require('path');
+const getPackageInfos = require('./getPackageInfos');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
-/**
- * @param {vscode.ExtensionContext} context
- */
+const setDecorations = (context) => {
+  const { workspace } = vscode;
+  const editor = vscode.window.activeTextEditor;
+  const fileName = editor.document.fileName;
+  if (fileName === path.join(workspace.rootPath, 'package.json')) {
+    const documentText = editor.document.getText();
+    const packageInfos = getPackageInfos();
+
+    for (let packageName in packageInfos) {
+      const packageInfo = packageInfos[packageName];
+
+      if (packageInfo.local !== packageInfo.satisfying) {
+        const matched = documentText.match(packageName);
+        if (matched && matched.index) {
+          const start = editor.document.positionAt(matched.index);
+          const end = editor.document.positionAt(matched.index + packageName.length);
+
+          const range = new vscode.Range(start, end);
+          editor.setDecorations(
+            vscode.window.createTextEditorDecorationType({
+              backgroundColor: '#e454541b',
+              gutterIconSize: '100%',
+              gutterIconPath: context.asAbsolutePath(path.join('assets', 'dark', 'error.svg')),
+              after: {
+                color: '#ff6464',
+                backgroundColor: 'fff0',
+              },
+              light: {
+                backgroundColor: '#e4545420',
+                gutterIconSize: '100%',
+                gutterIconPath: context.asAbsolutePath(path.join('assets', 'light', 'error.svg')),
+                after: {
+                  color: '#e45454',
+                },
+              },
+              isWholeLine: true,
+            })
+            , [{ range, renderOptions: { after: { color: '#ff6464', contentText: 'haha' } } }]
+          );
+          break;
+        }
+      }
+    }
+
+    // console.log(getPackageInfos());
+  }
+};
+
 function activate(context) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "vscode-package-version-helper" is now active!');
+  vscode.window.onDidChangeActiveTextEditor(() => {
+    setDecorations(context);
+  }, null, context.subscriptions);
+  setDecorations(context);
+};
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('extension.helloWorld', function() {
-    // The code you place here will be executed every time your command is executed
-
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World!');
-  });
-
-  context.subscriptions.push(disposable);
-}
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
-function deactivate() {}
 
-module.exports = {
-  activate,
-  deactivate
-};
