96e0a4e5704637dd3587e5a0ff70a9107c9055e0
yangfan <18767120422@163.com>
2020-04-22 15:43:45 +0800

    feat: add command

--- extensions/vscode-package-version-helper/src/index.js
+++ extensions/vscode-package-version-helper/src/index.js
@@ -3,9 +3,14 @@ const Checker = require('./Checker');
 
 function activate(context) {
   const checker = new Checker(context);
+  // Listen active file change
   vscode.window.onDidChangeActiveTextEditor(() => {
     checker.check();
   }, null, context.subscriptions);
+  // Add command
+  context.subscriptions.push(vscode.commands.registerCommand('package.version.check', function() {
+    checker.check();
+  }));
   checker.check();
 };
 
